952a32b968deea40a97fdffe810338c41723ed7c
Nupur Joshi <nupurj24@email.unc.edu>
2022-10-12 21:49:26 -0400

    Progress on learning how to use @State and Buttons

---
+++ primer02-buttons/primer02-buttons/primer02_buttonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  primer02_buttonsApp.swift
+//  primer02-buttons
+//
+//  Created by Nupur Joshi on 10/12/22.
+//
+
+import SwiftUI
+
+@main
+struct primer02_buttonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
